1e32be3fc27dbc14353d702ea3883e5a592cb7a6
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-11-17 16:45:14 +0530

    completed the draft tab main view

--- src/components/DraftAndSent/DraftTabSection.js
+++ src/components/DraftAndSent/DraftTabSection.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Tabs from "@mui/material/Tabs";
+import TabList from '@mui/lab/TabList';
 import Tab from "@mui/material/Tab";
+import TabPanel from '@mui/lab/TabPanel';
+import TabContext from '@mui/lab/TabContext';
 
 import { createTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@mui/material/styles";
@@ -13,28 +15,49 @@ const theme = createTheme({
 });
 
 function DraftTabSection() {
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className="pl-1 pr-5 bg-[#1A1D21] flex">
-        <Tabs
-          style={{ height: 20 }}
-          textColor="secondary"
-          indicatorColor="secondary"
-          aria-label="basic tabs example"
-        >
-          <Tab style={{ fontSize: 11 }} className="text-sm" label="Drafts" />
-          <Tab style={{ fontSize: 11 }} label="Scheduled" />
-          <Tab style={{ fontSize: 11 }} label="Sent" />
-        </Tabs>
-      </div>
+      <TabContext value={value}>
+        <div className="pl-1 pr-5 bg-[#1A1D21] flex">
+          <TabList 
+            style={{ height: 20 }}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="basic tabs example"
+            onChange={handleChange}
+          >
+            <Tab style={{ fontSize: 11 }} value="1" className="text-sm" label="Drafts" />
+            <Tab style={{ fontSize: 11 }} value="2" label="Scheduled" />
+            <Tab style={{ fontSize: 11 }} value="3" label="Sent" />
+          </TabList>
+        </div>
 
-      <div
-        style={{ height: "calc(100vh - 90px)" }}
-        className="w-[100%] bg-[#222529] px-6 py-4"
-      >
-        sda
-      </div>
+        <div
+          style={{ height: "calc(100vh - 90px)" }}
+          className="w-[100%] bg-[#222529]"
+        >
+          <TabPanel value="1">
+          
+          </TabPanel>
+          
+          <TabPanel value="2">
+          
+          </TabPanel>
+          
+          <TabPanel value="3">
+          
+          </TabPanel>
+        
+          </div>
+      </TabContext>
     </ThemeProvider>
   );
 }
