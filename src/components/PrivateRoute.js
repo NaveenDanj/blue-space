61c29c1a6b5a57e1b69330bcdfc460032192a169
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-11-25 16:58:17 +0530

    added private route changes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,12 +3,8 @@ import { Navigate , Outlet } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider'
 
 function PrivateRoute() {
-  
     const {currentUser} = useContext(AuthContext);
-    
     return currentUser ? <Outlet /> : <Navigate to="/auth/login" />;
-
-
 }
 
 export default PrivateRoute
